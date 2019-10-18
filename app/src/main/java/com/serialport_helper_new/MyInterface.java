34ea493fdfee5eb39f5a72b00bc5a9df96278f69
SpeedataGroup <shiyao.wang@speedatagroup.com>
2019-10-18 11:07:57 +0800

    Update Device Lib to 1.68

---
+++ app/src/main/java/com/serialport_helper_new/MyInterface.java
@@ -0,0 +1,12 @@
+package com.serialport_helper_new;
+
+interface MyInterface {
+	public void setSerialPort(String path);
+
+	public void setBaurdrate(int baurdrate);
+
+	public void setBaurdrateSpinner(int position);
+
+	public void setSerialPortSpinner(int position);
+
+}
